f66b5e694f7ec510bad73972b6fb3ece6f6c3402
eahmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-10-06 01:18:43 +0300

    update podfile for Example app

---
+++ Example/Pods/ValifyCapture/ValifyCapture/Classes/CameraDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CameraViewModelDelegate.swift
+//  ValifyCapture
+//
+//  Created by iOSAYed on 05/10/2024.
+//
+
+import Foundation
+
+public protocol CameraDelegate: AnyObject {
+    func didCaptureImage(_ image: UIImage)
+    func didFailWithError(_ error: Error)
+}
