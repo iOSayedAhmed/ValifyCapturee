a4da98b1d39796b1a4abda07902b27c0b14926fc
eahmed <60572628+iOSayedAhmed@users.noreply.github.com>
2024-10-06 00:27:52 +0300

    Reinstall ValifyCapture Pod from CocoaPods not Local

---
+++ Example/Pods/ValifyCapture/ValifyCapture/Classes/SelfiCapture.swift
@@ -0,0 +1,21 @@
+//
+//  SelfiCapture.swift
+//  ValifyCapture
+//
+//  Created by iOSAYed on 05/10/2024.
+//
+
+import Foundation
+
+public class SelfiCapture {
+    
+    public init(){}
+    
+    public func sayHello() {
+        print("Hello")
+    }
+    
+    func privateSayHello(){
+        print("Private")
+    }
+}
